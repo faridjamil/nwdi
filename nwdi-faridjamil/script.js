c295ea18dba1f5f888e2f79ac20f4108f9bb065a
Farid Jamil <89650023+faridjamil@users.noreply.github.com>
2025-09-08 06:14:53 +0000

    Delete nwdi-faridjamil directory

--- nwdi-faridjamil/script.js
+++
@@ -1,5 +0,0 @@
-function openMenu(menuName) {
-  // alert("Menu dibuka: " + menuName);
-  // Di sini kamu bisa arahkan ke halaman lain atau tampilkan konten dinamis
-  window.location.href = menuName + ".html";
-}
