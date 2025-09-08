4384e0dd1429395d539afcc744d17ee42871e420
Farid Jamil <89650023+faridjamil@users.noreply.github.com>
2025-09-08 06:14:07 +0000

    Add files via upload

---
+++ nwdi-faridjamil/script.js
@@ -0,0 +1,5 @@
+function openMenu(menuName) {
+  // alert("Menu dibuka: " + menuName);
+  // Di sini kamu bisa arahkan ke halaman lain atau tampilkan konten dinamis
+  window.location.href = menuName + ".html";
+}
